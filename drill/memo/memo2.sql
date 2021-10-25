b2c557e9b87ce087947809de42094d40ff964b0f
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-25 16:58:38 +0900

    [練習問題]

---
+++ drill/memo/memo2.sql
@@ -0,0 +1,26 @@
+--ASによる別名の定義
+SELECT 費目 AS ITEM , 入金額 AS RECEIVE, 出金額 AS PAY
+  FROM 家計簿　AS MONEYBOOK
+  WHERE 費目 = '給料'
+
+  --条件付きのUPDATE文
+  UPDATE 家計簿 
+   SET　入金額 = 99999
+   WHERE 日付　= '2018-02-03'
+
+   --WHEREのないUPDATE文は全権更新
+
+   --DELETE文の基本構文
+
+   DELETE 
+    FROM テーブル名
+   (WHERE) 
+
+    --WHEREのないUPDATE文は全権更新
+
+--INSERT文の基本構文
+--テーブルに新しいデータを追加するための命令。テーブルの行を指定する仕組みWHEREはない
+--その代わり、どこに、どのようなデータを追加するのかをしてする構造
+
+INSERT INTO テーブル名
+ VALUES　(値１、値２、値３)
