60dbcf0fdddb1afa7088aa624987f330f752ab2d
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-26 12:06:50 +0900

    [ANY / AND  演算子]

--- drill/memo/memo3.sql
+++ drill/memo/memo3.sql
@@ -22,4 +22,21 @@ SELECT * FROM 家計簿
    FROM 家計簿
  WHERE 出金額 BETWEEN 100 AND 3000 
 
- --IN
+ -- IN/NOT IN 演算子
+IN 演算子は、値がかっこ内に列挙した複数の値のいずれか合致するかを判定する演算子です。
+
+式 IN (値１、値２,値3)
+
+SELECT * 
+ FROM 家計簿
+ WHERE 費目 IN ('食費','交際費')
+
+SELECT *
+ FROM 家計簿
+ WHERE  費目 NOT IN ('食費','交際費')
+
+
+--AND 演算子 OR演算子
+
+条件式1 AND 条件式２
+条件式１ OR 条件式2
