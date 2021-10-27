d4720558f5403d87fed2fd3cdd9b5038595df8f4
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-27 14:12:01 +0900

    [まとめ]

--- drill/memo/memo4.sql
+++ drill/memo/memo4.sql
@@ -97,3 +97,30 @@ SELECT DISTINCT 列名・・・
     SELECT 費目 FROM 家計簿
     INTERSECT
     SELECT 費目 FROM 家計簿アーカイブ
+    
+    --重複を除外
+    SELECT DISTINCT 費目 FROM 家計簿
+　　
+    --3月に使った金額を大きい順に取り出す
+    SELECT * FROM 家計簿
+     WHERE 日付 >= '2018-03-01'
+       AND 日付 <= '2018-03-31'
+     ORDER BY 出金額　DESC  
+
+    --これまでの給料を大きい順に５件取り出す
+    SELECT * FROM
+     WHERE 費目 = '給料' ORDER BY 入金額　DESC
+     OFFSET 0 ROWS FETCH NEXT 5 ROWS ONLY
+
+     --家計簿と、アーカイブにある2月のデータをまとめて日付順に取り出したい
+     SELECT * FROM 家計簿
+     UNION
+     SELECT * FROM 家計簿アーカイブ
+     WHERE 日付 >= '2018-02-01'
+       AND 日付 <= '2018-02-28'
+     ORDER BY 1
+
+     --今月初めて発生した費目を知りたい
+     SELECT 費目 FROM 家計簿
+     EXCEPT
+     SELECT  費目 FROM 家計簿アーカイブ
