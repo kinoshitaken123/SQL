98e57478b9901c4eada72fa7ac33d3c8ca48c706
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-27 12:18:41 +0900

    [ORDER BY]

--- drill/memo/memo4.sql
+++ drill/memo/memo4.sql
@@ -12,3 +12,19 @@ SELECT文に付加すると、結果ひょうの中で内容が重複してい�
 
 SELECT DISTINCT 列名・・・
  FROM テーブル名
+
+ --ORDER BY 指定した列の値を基準として、検索結果を並べて取得することができる。
+  SELECT 列名・・・　FROM テーブル名
+  　ORDER BY 列名　並び順
+
+  --昇順 ASC 降準 DESC
+  --ORDER BY 句の初期値は昇順
+
+  --出金額で昇順となるよう並び替えて取得する
+  SELECT * FROM 家計簿
+  　ORDER BY 出金額
+
+  --複数の列で並び替える
+
+  SELECT * FROM 家計簿
+   ORDER BY 入金額 DESC, 出金額 DESC
