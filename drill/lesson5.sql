d76d5aaa6bf5b9c1b6617c4265c1b7582a0214bf
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-29 13:04:44 +0900

    [5-2]

--- drill/lesson5.sql
+++ drill/lesson5.sql
@@ -24,3 +24,23 @@ SELECT 受験者ID AS 合格者ID
  WHERE 午前 >= 60
    AND 午前１ + 午前２ >= 120
 　　AND 0.3 * (午前　+ 午後１ + 午後２ + 論述)　<= 論述
+
+--5-2
+1. UPDATE 回答者
+ 　SET 国名 = CASE SUBSTRING(TRIM(メールアドレス)),
+                          LENGTH(TRIM(メールアドレス))-1,2)
+                WHEN 'jp' THEN '日本'
+                WHEN 'uk' THEN 'イギリス'
+                WHEN 'cn' THEN '中国' 
+                WHEN 'fr' THEN 'フランス'  
+                WHEN 'vn' THEN 'ベトナム' END     
+                      
+2. SELECT TRIM(メールアドレス) AS メールアドレス,
+    CASE WHEN 年齢 >= 20 AND 年齢 < 30 THEN '20代'  
+         WHEN 年齢 >= 30 AND 年齢 < 40 THEN '30代'  
+         WHEN 年齢 >= 40 AND 年齢 < 50 THEN '40代'
+         WHEN 年齢 >= 50 AND 年齢 < 60 THEN '50代' END       
+    || ':' ||
+    CASE 性別　WHEN 'M' THEN '男性'
+              WHEN 'F' THEN '女性' END AS 男性
+    FROM 回答者          
