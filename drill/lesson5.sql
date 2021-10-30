d3fee287bcd1a10e6eda3964f8e8a200fdc54a4d
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-30 20:40:44 +0900

    [lesson5 終了]

--- drill/lesson5.sql
+++ drill/lesson5.sql
@@ -34,7 +34,7 @@ SELECT 受験者ID AS 合格者ID
                 WHEN 'cn' THEN '中国' 
                 WHEN 'fr' THEN 'フランス'  
                 WHEN 'vn' THEN 'ベトナム' END     
-                      
+
 2. SELECT TRIM(メールアドレス) AS メールアドレス,
     CASE WHEN 年齢 >= 20 AND 年齢 < 30 THEN '20代'  
          WHEN 年齢 >= 30 AND 年齢 < 40 THEN '30代'  
@@ -44,3 +44,26 @@ SELECT 受験者ID AS 合格者ID
     CASE 性別　WHEN 'M' THEN '男性'
               WHEN 'F' THEN '女性' END AS 男性
     FROM 回答者          
+
+--5-3
+
+1. UPDATE 受注
+　　　 SET 文字数　= LENGTH(REPLACE(文字数,'',''))
+
+2. SELECT 受注日, 受注ID, 文字数,
+          CASE COALESCE(書体コードl,'1')
+            WHEN '1' THEN 'ブロック体'
+            WHEN '2' THEN '筆記体'
+            WHEN '3' THEN '草書体' END AS 書体名,
+          CASE COALESCE (書体コード,'1')
+            WHEN '1' THEN 100
+            WHEN '2' THEN 150
+            WHEN '3' THEN 200 END AS 単価,
+          CASE WHEN 文字数 > 10 THEN 500
+          ELSE 0 END AS 特別加工料
+    FROM 受注    
+    ORDER BY 
+
+ 3 UPDATE 受注
+ 　　　SET 文字　= REPLACE(文字, '', '☆')
+   WHERE 受注ID '113'         
