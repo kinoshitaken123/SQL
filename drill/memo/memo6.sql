d3a274b12975677491bb68a5e3ec15e856f0f5c3
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-11-02 12:58:08 +0900

    [Having]

--- drill/memo/memo6.sql
+++ drill/memo/memo6.sql
@@ -14,3 +14,21 @@ SELECT COUNT(*) AS 食費の行動
  --NULLをぜろとして平均を求める
  SELECT AVG(COALESCE(出金額,0)) AS 出金額の平均
  　FROM 家計簿
+
+ --グループ化して集計する基本構文
+ SELECT グループ化の基準列名・・・、集計関数
+   FROM テーブル名
+   (WHERE 絞り込み条件)
+ GROUP BY グループ化の基準列名  
+
+ --グループ化してから絞り込む基本構文
+ SELECT グループ化の基準列名、集計関数
+   FROM テーブル名
+GROUP BY グループ化の基準列名
+  HAVING 集計結果に対する絞り込み条件   
+
+  --集計結果で絞り込む
+  SELECT 費目、SUM(出金額)　AS 費目別の出金額合計
+  　FROM　家計簿
+  GROUP BY 費目
+  　HAVING SUM(出金額)　> 0
