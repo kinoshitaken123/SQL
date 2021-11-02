80d9f01d370ef4304080d5600efc1b493f5f1eb0
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-11-02 16:54:10 +0900

    [lesson6-1]

---
+++ lesson6.sql
@@ -0,0 +1,26 @@
+--6-1
+--1日本全体としての年間降水量と、年間の最高気温、最低気温の平均
+SELECT SUM(降水量),MAX(最高気温),MIN(最低気温)
+FROM 都市別気象観測
+
+2 SELECT SUM(降水量),MAX(最高気温),MIN(最低気温)
+FROM 都市別気象観測
+WHERE 都市名 = '東京'
+
+3 SELECT 都市名,AVG(降水量),AVG(最高気温),AVG(最低気温)
+FROM 都市別気象観測
+GROUP BY 都市名
+
+4 SELECT 月,AVG(降水量),AVG(最高気温),AVG(最低気温)
+FROM 都市別気象観測
+GROUP BY 月
+
+5 SELECT 都市名,MAX(最高気温)
+  FROM 都市別気象観測 
+  GROUP BY 都市名
+  HAVING MAX (最高気温) >= 38
+
+6 SELECT 都市名,MIN(最低気温)
+  FROM 都市別気象観測 
+  GROUP BY 都市名
+  HAVING MIN(最低気温) <= -10
