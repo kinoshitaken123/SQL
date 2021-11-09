4a01dc7d175ccd003c6d4be35c168d2a79e774db
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-11-09 09:03:57 +0900

    [ロックの活用]

--- drill/memo/memo9.sql
+++ drill/memo/memo9.sql
@@ -8,3 +8,27 @@ COMMIT
 終了の指示。この指示までを一つのトランザクションとし、変更を確定する
 ROLLBACK
 終了の指示。この指示までを一つのトランザクションとし、変更の取り消しをする
+
+--2月以降の行をロックして集計する
+SELECT ~ FOR UPDATE (NOWAIT)
+
+BEGIN ;
+SELECT * FROM 家計簿
+ WHERE 日付 >= '2018-02-01'
+  FOR UPDATE ;
+ SELECT ~ ;
+ SELECT ~ ;
+ SELECT ~ ;
+ COMMIT ; 
+
+ --明示的な表ロックの取得
+ LOCK TABLE テーブル表 IN モード名 MODE
+
+ --MODE名はEXCLUSVE で排他ロック,SHAREで共有ロック
+
+ BEGIN;
+ LOCK TABLE 家計簿　IN EXCLUSIVE MODE ;
+ SELECT ~ ;
+ SELECT ~ ;
+ SELECT ~ ;
+ COMMIT;
