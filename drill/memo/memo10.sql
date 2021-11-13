84b65d8d1b865d67b632601c6985bc18a792fbd6
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-11-13 16:47:59 +0900

    [テーブル作成]

--- drill/memo/memo10.sql
+++ drill/memo/memo10.sql
@@ -6,3 +6,19 @@
 GRANT 権限名　TO ユーザー名
 REVOVE 権限名　FROM ユーザー名
 
+--テーブル作成
+CREATE TABLE テーブル名 (
+    列名１　列１の型名,
+    列名2　列2の型名,
+    :
+    列名X 列Xの型名
+)
+
+--家計簿テーブルを作成する
+CREATE TABLE 家計簿 (
+    日付　DATE,
+    費目ID INTEGER,
+    メモ　VARCHAR(100),
+    入金額　INTEGER,
+    出金額　INTEGER
+)
