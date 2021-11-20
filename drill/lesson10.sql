2f0990ec695b929f482f34626f992478f08b433f
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-11-20 15:56:55 +0900

    [10章問題終了]

---
+++ drill/lesson10.sql
@@ -0,0 +1,32 @@
+--10-1
+DDL命令の中からDDLに属するものを選ぶ
+DROP TABLE
+CREATE TABLE
+ALTER TABLE
+
+--10-2
+CREATE TABLE 学部(
+    ID CHAR(1) PRIMARY KEY,
+    名前 VARCHER(20) UNIQUE NOT NULL,
+    備考 VARCHER(100) DEFAULT '特になし' NOT NULL
+)
+
+--10-3
+CREATE TABLE 学生 (
+    学籍番号　CHAR(8) PRIMARY KEY,
+    名前 VARCHER(30) NOT NULL,
+    生年月日 DATE NOT NULL,
+    血液型　CHAR(2) CHECK(
+        血液型 IN ('A,'B','O','AB')OR
+        血液型 IS NULL
+    ),
+    学部ID CHAR(1) PREFERENCES 学部(ID)
+)
+
+--10-5
+BEGIN;
+UPDATE 学生 SET 学部ID 'K'
+ WHERE 学部ID ='R';
+ DELETE FROM 学部
+ WHERE ID = 'R';
+ COMMIT;
