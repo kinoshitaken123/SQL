f0b79607c27d8722dd7b20b720c46baab3cce3d4
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-12-02 18:16:39 +0900

    [DROP INDEX]

--- drill/memo/memo11.sql
+++ drill/memo/memo11.sql
@@ -6,3 +6,21 @@ CREATE INDEX 費目IDインデックス oN 家計簿 (費目ID);
 CREATE INDEX メモインデックス　ON 家計簿(メモ);
 
 --DROP INDEX インデックス名
+
+--WHERE句による絞り込み
+
+--完全一致検索
+
+SELECT * FROM 家計簿
+　WHERE メモ = '不明'
+
+--前方一致
+SELECT * FROM 家計簿
+ WHERE メモ　LIKE '一月の%'
+
+ ビューの作成にはCREATE VIEW文、削除にはDROP VIEW文
+ 
+ CREATE VIEW ビュー名 AS SEKECT文
+ DROP VIEW ビュー名
+
+ 
